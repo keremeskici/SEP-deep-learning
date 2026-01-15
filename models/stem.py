aa55e6ca970ea7f769090c5377413d544f24d72c
Zeynep Sürmen <zeynepsurmen@Zeyneps-MacBook-Air.local>
2026-01-15 15:31:59 +0100

    Add small-image stem

---
+++ models/stem.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+
+class SmallImageStem(nn.Module):
+    def __init__(self, in_channels: int = 3, out_channels: int = 64):
+        super().__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=False,
+        )
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
